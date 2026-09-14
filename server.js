412dd94910641a09763b9364a28cd589525b9484
MVDT PL <mvdtdeveloper@gmail.com>
2026-09-14 12:24:39 +0530

    update fixed

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(
 );
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("WhatsApp  with webhooks Assistant API is running ✅");
