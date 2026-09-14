6dbe5ab6bf3998f2bbe90d0d052ceb6f047f3b96
MVDT PL <mvdtdeveloper@gmail.com>
2026-09-14 12:14:58 +0530

    update fixed

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get("/webhook", (req, res) => {
 app.post("/webhook", (req, res)=>{
 
   console.log("run...........................")
-  // console.log(JSON.stringify( req.body, null, 2))
+  console.log(JSON.stringify( req.body, null, 2))
   res.status(200).send("webhook processed...")
 })
 // ================================
